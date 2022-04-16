3819c16250351472931233b076e3da7ef74bff88
Girish Parate <girishparate41@gmail.com>
2022-04-16 19:06:24 +0530

    reanimated added

--- android/app/src/main/java/com/starter/MainApplication.java
+++ android/app/src/main/java/com/starter/MainApplication.java
@@ -12,6 +12,9 @@ import java.lang.reflect.InvocationTargetException;
 import java.util.List;
 import com.microsoft.codepush.react.CodePush;
 
+import com.facebook.react.bridge.JSIModulePackage; 
+import com.swmansion.reanimated.ReanimatedJSIModulePackage; 
+
 public class MainApplication extends Application implements ReactApplication {
 
   private final ReactNativeHost mReactNativeHost =
@@ -31,6 +34,11 @@ public class MainApplication extends Application implements ReactApplication {
         @Override
         protected String getJSMainModuleName() {
           return "index";
+        }
+
+        @Override
+        protected JSIModulePackage getJSIModulePackage() {
+        return new ReanimatedJSIModulePackage();
         }
         
         @Override
--- src/screens/welcome/WelcomeScreen.tsx
+++ src/screens/welcome/WelcomeScreen.tsx
@@ -1,23 +1,111 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { StyleSheet, Text } from "react-native";
 import { AppView, Column } from "../../components/Flex/Flex";
 import { AppButton } from "@components/Button/Button";
 import '../../localization';
 import { useTranslation } from 'react-i18next';
+import Animated, { useSharedValue, useAnimatedStyle, interpolate, withTiming, withSpring, withDelay, withRepeat, withSequence, useAnimatedGestureHandler, useAnimatedScrollHandler } from "react-native-reanimated";
+import { PanGestureHandler } from "react-native-gesture-handler";
+
 export const WelcomeScreen = (props: any) => {
+
     const { t } = useTranslation();
+    const opacity = useSharedValue(.5)
+    const touchx = useSharedValue(0)
+    const touchY = useSharedValue(0)
+
+    const gestureHandler = useAnimatedGestureHandler({
+        onActive: (event) => {
+            touchx.value = event.translationX;
+            touchY.value = event.translationY;
+        },
+        onEnd: () => {
+            touchx.value = withSpring(0);
+            touchY.value = withSpring(0);
+        }
+    })
+
+    const scrollY = useSharedValue(0);
+
+
+    const scrollHandler = useAnimatedScrollHandler((event) => {
+        scrollY.value = event.contentOffset.y;
+    })
+
+    const animatedStyle = useAnimatedStyle(() => {
+        return {
+            opacity: opacity.value,
+            transform: [
+                // {
+                //     scale: interpolate(
+                //         opacity.value,
+                //         [0.5, 1],
+                //         [0.5, 1.2]
+                //     )
+                // }
+                { translateX: touchx.value },
+                { translateY: touchY.value }
+            ]
+        }
+    })
+
+    useEffect(() => {
+        //* withTiming
+        // opacity.value = withTiming(1, {
+        //     duration: 2000,
+        // })
+
+        //* withSpring 
+        // opacity.value = withSpring(1)
+
+        //* withDelay
+        // opacity.value = withDelay(2000, withSpring(1))
+
+        //* withRepeat
+        // opacity.value = withRepeat(
+        //     withSpring(1),
+        //     -1,
+        //     true
+        // )
+
+        //* withSequence
+        // opacity.value = withSequence(
+        //     withSpring(1),
+        //     withSpring(0.5),
+        //     withSpring(1)
+        // )
+    }, [])
+
+
+
     return (
         <AppView>
-            <Column alignItems="center" justifyContent="center" style={[style.container,]}>
-                <Text>homePage NS: {t('homePage:welcome')}</Text>
-                <Text>Default NS: {t('ok')}</Text>
-                <AppButton
-                    onPress={() => {
-                        console.log("React Native");
-                    }}>
-                    <Text>React Native Starter</Text>
-                </AppButton>
-            </Column>
+            <Animated.ScrollView onScroll={scrollHandler}
+                scrollEventThrottle={16}
+                style={{
+                    height: '100%',
+                    width: '100%',
+                }}
+                contentContainerStyle={{
+                    height: '100%',
+                    justifyContent: 'center'
+                }}
+            >
+                <PanGestureHandler onGestureEvent={gestureHandler}>
+                    <Animated.View style={[{ flex: 1 }, animatedStyle]}>
+                        <Column alignItems="center" justifyContent="center" style={[style.container,]}>
+                            <Text>homePage NS: {t('homePage:welcome')}</Text>
+                            <Text>Default NS: {t('ok')}</Text>
+                            <AppButton
+                                onPress={() => {
+                                    console.log("React Native");
+                                }}>
+                                <Text>React Native Starter</Text>
+                            </AppButton>
+                        </Column>
+                    </Animated.View>
+                </PanGestureHandler>
+            </Animated.ScrollView>
         </AppView>
     );
 }
