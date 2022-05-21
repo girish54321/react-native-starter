eacfba0c25fb3d8e260843843435900914dad787
Girish Parate <girishparate41@gmail.com>
2022-05-21 14:55:09 +0530

    clena up code

--- src/screens/tabScreen/tab1.tsx
+++ src/screens/tabScreen/tab1.tsx
@@ -1,42 +1,21 @@
 import * as React from 'react';
-import { Text, View, FlatList } from 'react-native';
+import { View } from 'react-native';
 import { createMaterialTopTabNavigator } from '@react-navigation/material-top-tabs';
 import { ListItem } from '@components/ListItem/ListItem';
-import Animated, { useSharedValue, useAnimatedStyle, interpolate, withTiming, withSpring, withDelay, withRepeat, withSequence, useAnimatedGestureHandler, useAnimatedScrollHandler, Extrapolate } from "react-native-reanimated";
+import Animated, { useSharedValue, useAnimatedStyle, interpolate, useAnimatedScrollHandler, Extrapolate } from "react-native-reanimated";
 const Tab = createMaterialTopTabNavigator();
 
 
 export const TabViewApp = () => {
-
-
     const scrollY = useSharedValue(0);
-    const opacity = useSharedValue(.5)
     const scrollHandler = useAnimatedScrollHandler((event) => {
         console.log(event);
 
         scrollY.value = event.contentOffset.y;
     })
-    // const scrollHandler = useAnimatedScrollHandler({
-    //     onScroll: (event) => {
-    //         // scrollY.value = event.contentOffset.y;
-    //     },
-    //     onBeginDrag: (event) => {
-    //         // isScrolling.value = true;
-    //         // scrollY.value = event.contentOffset.y;
-    //     },
-    //     onEndDrag: (event) => {
-    //         // isScrolling.value = false;
-    //         scrollY.value = event.contentOffset.y;
-    //     },
-    // });
 
     const animatedStyle = useAnimatedStyle(() => {
         return {
-            // opacity: interpolate(
-            //     scrollY.value,
-            //     [20, 200],
-            //     [1, 0.5, 0]),
-
             backgroundColor: 'red',
             height: interpolate(
                 scrollY.value,
@@ -44,7 +23,6 @@ export const TabViewApp = () => {
                 [280, 0],
                 {
                     extrapolateLeft: Extrapolate.CLAMP
-                    // extrapolateRight: Extrapolate.EXTEND
                 }
             ),
             transform: [
