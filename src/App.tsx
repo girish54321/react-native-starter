77adc03d17779de45b4457b304450a12705ffd4c
Girish Parate <girishparate41@gmail.com>
2022-06-25 18:31:08 +0530

    toolkit added

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,26 @@
 import React, { FC } from "react";
-import { Navigation } from "./navigation/mainNavigation";
-import ReduxThunk from 'redux-thunk'
-import { Provider, } from 'react-redux'
-import { applyMiddleware, createStore } from 'redux'
-import rootReducer from './redux/rootReducer'
-const store = createStore(rootReducer, applyMiddleware(ReduxThunk))
+import { createStackNavigator } from "@react-navigation/stack";
+import { NavigationContainer } from "@react-navigation/native";
+const Stack = createStackNavigator();
+import { Provider } from 'react-redux'
+import { configureStore } from '@reduxjs/toolkit'
+import { Home } from "newHome";
+import counterReducer from "couterSlice";
+export const store = configureStore({
+    reducer: {
+        counter: counterReducer,
+    },
+})
 
 export const App: FC = () => {
     return (
         <Provider store={store}>
-            <Navigation />
+            <NavigationContainer>
+                {/* <Navigation /> */}
+                <Stack.Navigator>
+                    <Stack.Screen name={"Home"} component={Home} />
+                </Stack.Navigator>
+            </NavigationContainer>
         </Provider>
     );
 }
---
+++ src/newHome.tsx
@@ -0,0 +1,12 @@
+import React, { FC } from "react";
+import { View } from "react-native";
+import { Headline } from "react-native-paper";
+
+export const Home = () => {
+    return (
+        <View style={{ flex: 1 }}>
+            <Headline>Home Name</Headline>
+        </View>
+    )
+}
+
