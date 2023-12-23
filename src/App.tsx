1b61fe794ddc05612ba674730b88ce9f589a21a9
Girish Parate <girishparate@Girishs-iMac.local>
2023-12-23 16:03:19 +0530

    dropdoen added

--- src/App.tsx
+++ src/App.tsx
@@ -1,117 +1,121 @@
-// import React, { FC } from "react";
-// import { Navigation } from "./navigation/mainNavigation";
-// import { Provider, } from 'react-redux'
-// import { store } from './redux/rootReducer'
+import React, { FC } from "react";
+import { Navigation } from "./navigation/mainNavigation";
+import { Provider, } from 'react-redux'
+import { store } from './redux/rootReducer'
+import { AutocompleteDropdownContextProvider } from "react-native-autocomplete-dropdown";
 
-// export const App: FC = () => {
-//     return (
-//         <Provider store={store}>
-//             <Navigation />
-//         </Provider>
-//     );
-// }
-
-
-import React, { useCallback, useRef, useMemo } from "react";
-import { StyleSheet, View, Text, Button, ScrollView } from "react-native";
-import BottomSheet, { BottomSheetScrollView, BottomSheetTextInput } from "@gorhom/bottom-sheet";
-import { TextInput } from "react-native-paper";
-
-export const App = () => {
-    // hooks
-    const sheetRef = useRef<BottomSheet>(null);
-
-    // variables
-    const data = useMemo(
-        () =>
-            Array(50)
-                .fill(0)
-                .map((_, index) => `index-${index}`),
-        []
-    );
-    const snapPoints = useMemo(() => ["25%", "50%", "90%"], []);
-
-    // callbacks
-    const handleSheetChange = useCallback((index) => {
-        console.log("handleSheetChange", index);
-    }, []);
-    const handleSnapPress = useCallback((index) => {
-        // sheetRef.current?.snapToIndex(index);
-        sheetRef.current?.expand();
-    }, []);
-    const handleClosePress = useCallback(() => {
-        sheetRef.current?.close();
-    }, []);
-
-    // render
-    const renderItem = useCallback(
-        (item, index) => (
-            <View key={item} style={styles.itemContainer}>
-                {index === 0 ? <TextInput style={styles.input} /> : null}
-                <Text>{item}</Text>
-                <Text>{`${index}`}</Text>
-            </View>
-        ),
-        []
-    );
+export const App: FC = () => {
     return (
-        <View style={styles.container}>
-            <ScrollView>
-                {[1, 2, 3, 4, 5, 6, 7, 8, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9].map((data) => {
-                    return (
-                        <View style={{ margin: 8 }} >
-                            <TextInput style={{ margin: 8 }} />
-                            <Text onPress={() => {
-                                sheetRef.current.expand()
-                            }}>Open</Text>
-                            <Text onPress={() => {
-                                sheetRef.current.close()
-                            }}>Close</Text>
-                        </View>
-                    )
-                })}
-            </ScrollView>
-            {/* <Button title="Snap To 90%" onPress={() => handleSnapPress(2)} /> */}
-            {/* <Button title="Snap To 50%" onPress={() => handleSnapPress(1)} />
-            <Button title="Snap To 25%" onPress={() => handleSnapPress(0)} /> */}
-            {/* <Button title="Close" onPress={() => handleClosePress()} /> */}
-            <BottomSheet
-                ref={sheetRef}
-                // index={1}
-                // index={0}
-                enablePanDownToClose={true}
-                // snapPoints={snapPoints}
-                enableDynamicSizing={true}
-                onChange={handleSheetChange}
-            >
-                <BottomSheetScrollView contentContainerStyle={styles.contentContainer}>
-                    {data.map(renderItem)}
-                </BottomSheetScrollView>
-            </BottomSheet>
-        </View>
+        <Provider store={store}>
+            {/* <Navigation /> */}
+            <AutocompleteDropdownContextProvider>
+                <Navigation />
+            </AutocompleteDropdownContextProvider>
+        </Provider>
     );
-};
+}
 
-const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        // paddingTop: 200,
-    },
-    contentContainer: {
-        backgroundColor: "white",
-    },
-    itemContainer: {
-        padding: 6,
-        margin: 6,
-        backgroundColor: "#eee",
-    },
-    input: {
-        marginTop: 8,
-        marginBottom: 10,
-        borderRadius: 10,
-        fontSize: 16,
-        lineHeight: 20,
-        padding: 8,
-        backgroundColor: 'rgba(151, 151, 151, 0.25)',
-    },
-});
+
+// import React, { useCallback, useRef, useMemo } from "react";
+// import { StyleSheet, View, Text, Button, ScrollView } from "react-native";
+// import BottomSheet, { BottomSheetScrollView, BottomSheetTextInput } from "@gorhom/bottom-sheet";
+// import { TextInput } from "react-native-paper";
+
+// export const App = () => {
+//     // hooks
+//     const sheetRef = useRef<BottomSheet>(null);
+
+//     // variables
+//     const data = useMemo(
+//         () =>
+//             Array(50)
+//                 .fill(0)
+//                 .map((_, index) => `index-${index}`),
+//         []
+//     );
+//     const snapPoints = useMemo(() => ["25%", "50%", "90%"], []);
+
+//     // callbacks
+//     const handleSheetChange = useCallback((index) => {
+//         console.log("handleSheetChange", index);
+//     }, []);
+//     const handleSnapPress = useCallback((index) => {
+//         // sheetRef.current?.snapToIndex(index);
+//         sheetRef.current?.expand();
+//     }, []);
+//     const handleClosePress = useCallback(() => {
+//         sheetRef.current?.close();
+//     }, []);
+
+//     // render
+//     const renderItem = useCallback(
+//         (item, index) => (
+//             <View key={item} style={styles.itemContainer}>
+//                 {index === 0 ? <TextInput style={styles.input} /> : null}
+//                 <Text>{item}</Text>
+//                 <Text>{`${index}`}</Text>
+//             </View>
+//         ),
+//         []
+//     );
+//     return (
+//         <View style={styles.container}>
+//             <ScrollView>
+//                 {[1, 2, 3, 4, 5, 6, 7, 8, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9].map((data) => {
+//                     return (
+//                         <View style={{ margin: 8 }} >
+//                             <TextInput style={{ margin: 8 }} />
+//                             <Text onPress={() => {
+//                                 sheetRef.current.expand()
+//                             }}>Open</Text>
+//                             <Text onPress={() => {
+//                                 sheetRef.current.close()
+//                             }}>Close</Text>
+//                         </View>
+//                     )
+//                 })}
+//             </ScrollView>
+//             {/* <Button title="Snap To 90%" onPress={() => handleSnapPress(2)} /> */}
+//             {/* <Button title="Snap To 50%" onPress={() => handleSnapPress(1)} />
+//             <Button title="Snap To 25%" onPress={() => handleSnapPress(0)} /> */}
+//             {/* <Button title="Close" onPress={() => handleClosePress()} /> */}
+//             <BottomSheet
+//                 ref={sheetRef}
+//                 // index={1}
+//                 // index={0}
+//                 enablePanDownToClose={true}
+//                 // snapPoints={snapPoints}
+//                 enableDynamicSizing={true}
+//                 onChange={handleSheetChange}
+//             >
+//                 <BottomSheetScrollView contentContainerStyle={styles.contentContainer}>
+//                     {data.map(renderItem)}
+//                 </BottomSheetScrollView>
+//             </BottomSheet>
+//         </View>
+//     );
+// };
+
+// const styles = StyleSheet.create({
+//     container: {
+//         flex: 1,
+//         // paddingTop: 200,
+//     },
+//     contentContainer: {
+//         backgroundColor: "white",
+//     },
+//     itemContainer: {
+//         padding: 6,
+//         margin: 6,
+//         backgroundColor: "#eee",
+//     },
+//     input: {
+//         marginTop: 8,
+//         marginBottom: 10,
+//         borderRadius: 10,
+//         fontSize: 16,
+//         lineHeight: 20,
+//         padding: 8,
+//         backgroundColor: 'rgba(151, 151, 151, 0.25)',
+//     },
+// });
--- src/screens/UsersScreen/SelectedUser.tsx
+++ src/screens/UsersScreen/SelectedUser.tsx
@@ -1,14 +1,15 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { StyleSheet, View, } from "react-native";
 import { ScrollView } from "react-native-gesture-handler";
 import { AppView } from "@components/Flex/Flex";
 import { Avatar, Text } from "react-native-paper";
 import { scale } from "Config/ScalingUtils";
 import { UserList } from "models/responseType/UserListResponse";
+import { AutocompleteDropdown } from 'react-native-autocomplete-dropdown';
 
 export const SelectedUserScreen = (props: any) => {
     const data: UserList = props?.route?.params?.data
-
+    const [selectedItem, setSelectedItem] = useState(null);
     useEffect(() => {
         props.navigation.setOptions({ title: `${data.first_name} ${data.last_name}` })
     }, [])
@@ -25,6 +26,22 @@ export const SelectedUserScreen = (props: any) => {
                     <Text variant="headlineLarge">{data.first_name} {data.last_name}</Text>
                     <Text variant="titleLarge">{data.email}</Text>
                 </View>
+                <AutocompleteDropdown
+                    clearOnFocus={false}
+                    // closeOnBlur={true}
+                    // ClearIconComponent={<Text></Text>}
+                    inputHeight={50}
+                    debounce={620}
+                    inputContainerStyle={{ marginVertical: 16, backgroundColor: '#fff', height: 50 }}
+                    closeOnSubmit={false}
+                    initialValue={'3'} // or just '2'
+                    onSelectItem={setSelectedItem}
+                    dataSet={[
+                        { id: '1', title: 'Alpha' },
+                        { id: '2', title: 'Beta' },
+                        { id: '3', title: 'Gamma' },
+                    ]}
+                />
             </ScrollView>
         </AppView>
     );
